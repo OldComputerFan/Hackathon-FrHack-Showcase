ad10db78c9509c87aec53e9f5accbbf7c935fb04
Jan <33702431+jahknem@users.noreply.github.com>
2025-04-06 14:16:03 +0200

    Create gps.py

---
+++ gps.py
@@ -0,0 +1,14 @@
+import ntplib
+from datetime import datetime, timedelta
+from config import SIMULATE_GNSS_FAILURE
+
+def get_gps_time():
+    if SIMULATE_GPS_FAILURE:
+        return "GPS error: Simulated interference"
+    try:
+        client = ntplib.NTPClient()
+        response = client.request('pool.ntp.org', version=3, timeout=1)
+        return datetime.fromtimestamp(response.tx_time) - timedelta(seconds=19)
+    except Exception as e:
+        return f"GPS error: {e}"
+        # Fallback to error message if any error occurs
